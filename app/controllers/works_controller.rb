a91fb7fdbe99e9ac4700c2f1d2ddcd1a0e51e632
Kelly Austin <kaustly@gmail.com>
2015-11-18 16:54:32 -0500

    initial commit

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,10 @@
+class WorksController < ApplicationController
+  # before_action :set_work, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    #@works = Work.all
+  end
+
+
+end
