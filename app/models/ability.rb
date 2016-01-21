fe10b4de193a7d984bdace929f5c0c65216e03b8
Kelly Austin <kaustly@gmail.com>
2016-01-21 14:50:07 -0500

    YAY! Still need a view

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Work
+    can [:read, :add_dig, :remove_dig], Work
+
 
     if user
       can :create, Work
-      can [:update, :destroy, :add_dig, :remove_dig], Work do |work|
+      can [:update, :destroy], Work do |work|
         user.id == work.user_id
       end
     end
